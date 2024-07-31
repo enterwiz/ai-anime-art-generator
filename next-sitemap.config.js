8db8706d228498845c2b0b41313947e8cd89915e
enterwiz <enterwizdev@outlook.com>
2024-07-31 11:14:07 +0800

    update default site url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://enterwiz.com",
+  siteUrl: process.env.SITE_URL || "https://enterwiz.org",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
